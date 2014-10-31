97e51facf4c1f7da1645748b47a8ccc9002daaf0
Triangle717 <le717.code@yahoo.com>
2014-10-30 21:27:07 -0400

    Multiple things:
    + Add command-line usage
    + Ability to parse more than the first 10 lines
    + Begin docstrings
    * Bug fixes
    - Remove hardcoded I/O

--- LR-MVE.py
+++ LR-MVE.py
@@ -27,36 +27,36 @@ import sys
 
 class LRMVE(object):
 
-    def __init__(self, inFile, outFile):
+    def __init__(self, axis, changeValue, inFile, outFile):
 
-        print("LEGO Racers - Mass Value Editor\n")
-        self.axis = "y"
-        self.changeValue = 20
+        self.axis = axis
         self.inFile = os.path.abspath(inFile)
         self.outFile = os.path.abspath(outFile)
+        self.timesChanged = 0
         self.__fileContent = self._readFile()
         self.__prefixRegex = re.compile(r"(float|byte)")
         self.__commentRegex = re.compile(r"//.*")
-        self.__positions = {
-            "x": 1,
-            "y": 2,
-            "z": 3,
-            "tu": 2,
-            "tv": 4,
-            "r": 5,
-            "g": 6,
-            "b": 7,
-            "a": 8
-        }
+
+        # Determine if the multipler is an integer or float
+        if changeValue.find(".") > -1:
+            self.changeValue = float(changeValue)
+        else:
+            self.changeValue = int(changeValue)
 
     def _readFile(self):
+        """Write the source file.
 
+        @return {array} Array contaning contents of source file.
+        """
         with open(self.inFile, "rt") as f:
             lines = f.readlines()
         return lines
 
     def writeFile(self):
+        """Write the destination file.
 
+        @return {boolean} Always returns True.
+        """
         with open(self.outFile, "wt") as f:
             f.write("".join(self.__fileContent))
         return True
@@ -79,10 +79,16 @@ class LRMVE(object):
 
             # Make it an integer for math(s) operations
             value = int(value)
-        return [text, value, comment]
+            return [text, value, comment]
+        return False
 
     def _joinLine(self,  parts, pos):
+        """TODO.
 
+        @param {array} parts TODO.
+        @param {integer} pos The line number to update with the changed value.
+        @return {boolean} Always returns True.
+        """
         # Join the parts
         newLine = "{0}{1}".format(parts[0], parts[1])
 
@@ -98,43 +104,104 @@ class LRMVE(object):
         return True
 
     def changeValues(self):
+        """TODO.
 
+        @return {boolean} Always returns True.
+        """
         def _doMaths(value, i):
-            # Perform the math(s) operation
-            # Addition
-            if self.changeValue >= 0:
-                value += self.changeValue
+            """Perform the math(s) operation."""
+            return value + self.changeValue
 
-            # Subtraction
-            else:
-                value -= self.changeValue
-            return value
+        positions = {"x": 1,
+                     "y": 2,
+                     "z": 3,
+                     "tu": 4,
+                     "tv": 5,
+                     "r": 6,
+                     "g": 7,
+                     "b": 8,
+                     "a": 9
+                     }
 
         # Scan just the first 10 lines
         for i in range(1, 10):
             parts = self._splitLine(self.__fileContent[i])
 
             # Make sure we are on the correct line before math(s)
-            if i == self.__positions[self.axis]:
+            if i == positions[self.axis]:
                 parts[1] = _doMaths(parts[1], i)
 
             # Merge the parts back together
             self._joinLine(parts, i)
+            self.timesChanged += 1
 
         # Now do the rest of the lines using the same process
-        for i in range(11, len(self.__fileContent), 9):
+        for i in range(9 + positions[self.axis], len(self.__fileContent), 9):
             parts = self._splitLine(self.__fileContent[i])
-            parts[1] = _doMaths(parts[1], i)
-            self._joinLine(parts, i)
+            if parts:
+                parts[1] = _doMaths(parts[1], i)
+                self._joinLine(parts, i)
+                self.timesChanged += 1
+        return True
 
-    def _commandLine(self):
-        """Command-line arguments parser."""
-        pass
+
+def commandLine():
+    """Command-line arguments parser.
+
+    @return {tuple|Boolean} A four index tuple containing the parameters given,
+        False if all arguments were not passed or the help was invoked.
+    """
+    def cmdHelp():
+        """Print command-line arguments help text."""
+        print("""LR-MVE Command-line Usage
+
+{0} [Axis] [Change value] [Input file] [Output file]
+
+
+Axis: The axis you want to edit.
+    Values are x, y, z, tu, tv, r, g, b, and a.
+Change value: The positive or negative value of your desired change.
+Input file: Text file containing decoded GDB format structure,
+    as decompiled using the LR Binary File Viewer.
+Output file: Destination text file for changed values.
+""".format(os.path.basename(__file__)))
+        return False
+
+    # No arguments were given
+    try:
+        sys.argv[1]
+    except IndexError:
+        cmdHelp()
+        return False
+
+    # The help parameter was given
+    if sys.argv[1] in ("-h", "--help"):
+        cmdHelp()
+        return False
+
+    # All the arguments required were not given
+    try:
+        axis = sys.argv[1]
+        changeValue = sys.argv[2]
+        inFile = sys.argv[3]
+        outFile = sys.argv[4]
+        return (axis, changeValue, inFile, outFile)
+    except IndexError:
+        cmdHelp()
+        return False
+
+
+def main():
+    """Entry point for entire program."""
+    print("\nLEGO Racers - Mass Value Editor\n")
+    arguments = commandLine()
+    if arguments:
+        lrmve = LRMVE(arguments[0], arguments[1], arguments[2], arguments[3])
+        lrmve.changeValues()
+        lrmve.writeFile()
+        print("{0} updated values saved to {1}".format(
+              lrmve.timesChanged, arguments[3]))
 
 
 if __name__ == "__main__":
-    inFile = "Testing/original.txt"
-    outFile = "Testing/updated.txt"
-    editor = LRMVE(inFile, outFile)
-    editor.changeValues()
-    editor.writeFile()
+    main()
