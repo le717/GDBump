0d5aa55d5158c7819a78fcacd19ebf866fdb325a
Triangle717 <le717.code@yahoo.com>
2014-11-17 18:24:04 -0500

    Add new test

--- tests/unittests.py
+++ tests/unittests.py
@@ -31,7 +31,7 @@ class TestRunner(object):
         if self.axis != "" and self.changeValue != "":
             self.changeTest(self.changeValue, self.msg)
 
-    def changeTest(self, changeValue, msg, axis=None):
+    def changeTest(self, changeValue, msg, axis=None, inFile=None):
         self.msg = msg
         self.changeValue = changeValue
 
@@ -39,9 +39,13 @@ class TestRunner(object):
         if axis is not None or (type(axis) == str and axis != self.axis):
             self.axis = axis
 
+        # Update the input file only when needed
+        if inFile is not None and (inFile != self.inFile):
+            self.inFile = inFile
+
         # Create an instance of GDBump
         self.gdbump = GDBump.GDBump(self.axis, self.changeValue,
-                                    self.inFile, self.outFile)
+                                    self.inFile, self.outFile, test=True)
         self.gdbump.processFile()
 
     def processLines(self, shouldEqual):
@@ -65,7 +69,16 @@ def main():
     outFile = os.path.join(testDir, "y-axis-changed.txt")
     testRunner = TestRunner(inFile, outFile, "y")
 
-    testRunner.changeTest(20, "should increase the values by 20")
+    try:
+        # Make sure input file exists
+        testRunner.changeTest(20, "should error from non-existant file",
+                              inFile=os.path.join(testDir, "y-axis-not.txt"))
+        testRunner.processLines((0, 0, 0, 0))
+    except FileNotFoundError:
+        testRunner.testsPassed += 1
+
+    testRunner.changeTest(20, "should increase the values by 20",
+                          inFile=inFile)
     testRunner.processLines((0, -30, 0, -40))
 
     testRunner.changeTest(-20, "should decrease the values by 20")
@@ -74,19 +87,18 @@ def main():
     testRunner.changeTest("~20", "should replace all values with 20")
     testRunner.processLines((20, 20, 20, 20))
 
-    # Write the file and confirm it's length
+    # Write the output file and confirm it's length
     testRunner.gdbump.writeFile()
     if os.path.isfile(outFile):
         with open(outFile, "rt") as f:
             numOfLines = f.readlines()
 
-        print("\nThere should {0} lines in the output file.".format(len(numOfLines) + 1))
+        print("\nThere should {0} lines in the output file.".format(
+              len(numOfLines) + 1))
         assert len(numOfLines) == 38, False
         testRunner.testsPassed += 1
 
     print()
     print("*" * 7, "{0} tests passed.".format(testRunner.testsPassed), "*" * 7)
 
-
-# Unconditionally run tests
 main()
